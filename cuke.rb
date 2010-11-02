caa39b43d3b65b223963656b7f1bda949fdf3de2
Les Hill <leshill@gmail.com>
2010-11-02 11:14:19 -0400

    Put rspec-rails in dev gems for rake spec

--- cuke.rb
+++ cuke.rb
@@ -11,8 +11,8 @@ gem "factory_girl_rails", ">= 1.0.0", :group => [:test, :cucumber]
 gem "factory_girl_generator", ">= 0.0.1", :group => [:test, :cucumber, :development]
 gem "haml-rails", ">= 0.3.4"
 gem "launchy", ">= 0.3.7", :group => [:test, :cucumber]
-gem "rspec-rails", ">= 2.0.1", :group => [:test, :cucumber]
 gem "spork", ">= 0.8.4", :group => [:test, :cucumber]
+gem "rspec-rails", ">= 2.0.1", :group => [:cucumber, :development, :test]
 
 generators = <<-GENERATORS
 
